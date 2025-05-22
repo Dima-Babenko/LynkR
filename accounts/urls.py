3cb83ac36521e7974d04791bf35c7cb0fc5bf290
Pushka <babenkodima312@gmail.com>
2025-05-22 22:35:53 +0300

    added simple friend system

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('logout/', views.logout_view, name='logout'),
     path('profile/', views.profile_view, name='profile'),
     path('profile/edit/', views.edit_profile_view, name='edit_profile'),
+    path('friends/', views.friends_view, name='friends'),
+    path('friends/send/', views.send_friend_request, name='send_friend_request'),
+    path('friends/respond/<int:request_id>/<str:action>/', views.respond_to_request, name='respond_friend_request'),
+    path('friends/change_id/', views.change_friend_id, name='change_friend_id'),
 ]
