8351c2723f81f7fbaf3c0cb87ced25a3719a4761
Pushka <babenkodima312@gmail.com>
2025-06-30 19:18:31 +0300

    exactly the last commit

--- config/settings.py
+++ config/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = 'django-insecure-6pwm@w(w!xy0290pzgst6&#f$eoj1d(ilcl6e6c*d0!+8j(azs
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.onrender.com']
+ALLOWED_HOSTS = ['.onrender.com', "127.0.0.1"]
 
 # Application definition
 INSTALLED_APPS = [
--- config/urls.py
+++ config/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', home, name='home'),
-    path('accounts/', include('accounts.urls')),
+    path('accounts/', include('accounts.urls', namespace='accounts')),
     path('posts/', include('posts.urls', namespace='posts')),
     path('chat/', include('chat.urls')),
     path('admin/', admin.site.urls),
